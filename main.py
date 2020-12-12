78b1d6cab71e7610a95625919732be58945f1a2c
Meirylindi <meirylindimlima@gmail.com>
2020-12-12 02:25:25 +0000

    Refiz o programa depois de assistir seus vídeos. Na verdade era para ter assistido antes de dar o primeiro commit & push. Mas foi mais forte que eu rsrs.

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 """Arquivo principal que será interpretado pelo interpretador."""
 import math
 
+METROS_POR_LITRO = 6
+LITROS_POR_LATA = 18
+LITROS_POR_GALAO = 3.6
+VALOR_LATA = 80
+VALOR_GALAO = 25
+FOLGA = 0.1
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    pintar_area = float(input('Digite a área: '))
-    lata_area = 18 * 6
-    galao_area = 3.6 * 6
-    total_area = pintar_area + (pintar_area * 0.1)
-    if total_area % lata_area < lata_area:
-      total_lata = (total_area//lata_area + 1)
-    else:
-      total_lata = total_area/lata_area
-    if total_area % galao_area < galao_area:
-      total_galao = (total_area//galao_area + 1)
-    else:
-      total_galao = total_area/galao_area
-    valor_lata = total_lata*80
-    valor_galao = total_galao*25
-    resto_lata = total_area % lata_area
-    if resto_lata < galao_area:
-      galao = 1
-      lata = total_lata - 1
-    elif resto_lata % galao_area < galao_area:
-      galao = resto_lata//galao_area + 1
-      lata = total_lata - 1
-    else:
-      galao = 0
-    print(f'O valor gasto comprando apenas latas é de R$ {valor_lata:.2f}.')
-    print(f'Serão necessárias {int(total_lata)} latas.')
-    print(f'O valor gasto comprando apenas galões é de R$ {valor_galao:.2f}.') 
-    print(f'Serão necessários {int(total_galao)} galões.')
-    print(f'O valor gasto comprando de forma que gere a menor quantidade de desperdício é de R$ {lata * 80 + galao * 25:.2f}.')
-    print(f'Serão necessárias {int(lata)} latas e {int(galao)} galões.')
 
+    # Entrada dos dados.
+    area = float(input('Digite a área: '))
     
+    # Manipulação
+    area_total = area * FOLGA + area
+    quantidade_litros = area_total / METROS_POR_LITRO
+    
+    total_latas = math.ceil(quantidade_litros / LITROS_POR_LATA)
+    preco_total_latas = total_latas * VALOR_LATA
+     
+    total_galoes = math.ceil(quantidade_litros / LITROS_POR_GALAO)
+    preco_total_galoes = total_galoes * VALOR_GALAO
+
+    resto_lata = quantidade_litros % LITROS_POR_LATA
+    total_galao = math.ceil(resto_lata / LITROS_POR_GALAO)
+    total_lata = math.floor(quantidade_litros / LITROS_POR_LATA)
 
 
+    menor_desperdicio = (total_lata * VALOR_LATA) + (total_galao * VALOR_GALAO)
+
+    # Exibição.
+    print(f'O valor gasto comprando apenas latas é de R$ {preco_total_latas:.2f}.')
+    print(f'Serão necessárias {total_latas} latas.')
+    print(f'O valor gasto comprando apenas galões é de R$ {preco_total_galoes:.2f}.')
+    print(f'Serão necessários {total_galoes} galões.')
+    print(f'O valor gasto comprando de forma que gere a menor quantidade de desperdício é de R$ {menor_desperdicio:.2f}.')
+    print(f'Serão necessárias {total_lata} latas e {total_galao} galões.')
 
 
 if __name__ == '__main__':
